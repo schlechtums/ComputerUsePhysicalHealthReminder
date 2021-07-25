bede7f047bed0df35c527634abb3b92b33fd46aa
Brian Adams <schlechtums@gmail.com>
2021-07-25 16:13:37 -0400

    MVP

---
+++ src/CUPHR.ViewModel/Types/Timers/Eyes202020Timer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CUPHR.ViewModel.Types.Timers
+{
+    public class Eyes202020Timer : TimerBase
+    {
+        public override String Name => "Eyes 20-20-20";
+
+        public override TimeSpan Interval => TimeSpan.FromMinutes(20);
+
+        public override String ExpirationMessage => "Focus on something 20 feet away for 20 seconds";
+    }
+}
---
+++ src/CUPHR.ViewModel/ViewModel.cs
@@ -0,0 +1,50 @@
+﻿using CUPHR.ViewModel.Types.Timers;
+using Schlechtums.Core.BaseClasses;
+using System;
+using System.Collections.Generic;
+
+namespace CUPHR.ViewModel
+{
+    public class ViewModel : ViewModelBase
+    {
+        public ViewModel()
+        {
+            this.Timers = new List<TimerBase>
+            {
+                new Eyes202020Timer()
+            };
+        }
+
+        public List<TimerBase> Timers { get; private set; }
+
+        public void StartAllTimers()
+        {
+            foreach (var t in this.Timers)
+            {
+                this.StartTimer(t);
+            }
+        }
+
+        public void StartTimer(TimerBase timer)
+        {
+            timer.Start();
+            timer.OnElapsed += this.OnTimerElapsed;
+        }
+
+        public void StopAllTimers()
+        {
+            foreach (var t in this.Timers)
+            {
+                this.StopTimer(t);
+            }
+        }
+
+        public void StopTimer(TimerBase timer)
+        {
+            timer.Stop();
+            timer.OnElapsed -= this.OnTimerElapsed;
+        }
+
+        public event TimerBase.OnElapsedHandler OnTimerElapsed;
+    }
+}
